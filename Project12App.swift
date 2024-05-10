b44b05b70f28251249e25b82d56a209f9cf967a3
HPopo <116526762+HPopo@users.noreply.github.com>
2024-05-09 23:20:50 -0500

    Add files via upload

---
+++ Project12App.swift
@@ -0,0 +1,17 @@
+//
+//  Project12App.swift
+//  Project12
+//
+//  Created by Hector Martinez on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Project12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
